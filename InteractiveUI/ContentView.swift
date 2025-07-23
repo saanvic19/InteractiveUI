4bb66a9023eb255fdc08ff6ae1ee3a913c5623e3
Saanvi <chsaanvi@gmail.com>
2025-07-23 16:14:41 -0500

    Added action into button

--- InteractiveUI/ContentView.swift
+++ InteractiveUI/ContentView.swift
@@ -4,7 +4,6 @@
 //
 //  Created by Saanvi Chiluveru on 7/23/25.
 //
-
 import SwiftUI
 
 struct ContentView: View {
@@ -14,19 +13,18 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
-            Text("What is your name?")
+            Text(textTitle)
                 .font(.title)
             TextField("Type your name here...", text: $name)
                 .multilineTextAlignment(.center)
                 .font(.title)
                 .border(Color.gray, width: 1)
             Button("Submit Name") {
-                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                textTitle = "Welcome, \(name)!"
             }
             .font(.title2)
             .buttonStyle(.borderedProminent)
             .tint(.purple)
-
         }
         .padding()
     }
